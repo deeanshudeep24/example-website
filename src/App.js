50feba1908777a9d6aaa595fba29e765057d5d67
deeanshudeep24 <thenewdate24@gmail.com>
2024-05-01 22:50:57 +0530

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import { Grid } from "@mui/material";
+import Particles from "react-tsparticles";
+
+function App() {
+  return (
+    <Grid container>
+      
+      <Home />
+    </Grid>
+  );
+}
+
+export default App;
